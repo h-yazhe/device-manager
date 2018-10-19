2cba38e3e7ceffb3a445d69f683623d436362194
Ricardo <dangguigxk@outlook.com>
2018-10-19 10:57:01 +0800

    将RepairOrder重新添加至git

---
+++ src/main/java/com/sicau/devicemanager/POJO/DO/RepairOrder.java
@@ -0,0 +1,40 @@
+package com.sicau.devicemanager.POJO.DO;
+
+import com.sicau.devicemanager.config.validation.group.DeviceValidatedGroup.SubmitRepairOrder;
+import lombok.Data;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+public class RepairOrder {
+
+    private Integer id;
+
+    /** 设备id */
+    @NotNull(message = "设备id不能为空",groups = {SubmitRepairOrder.class})
+    private String deviceId;
+
+    /** 申请人id */
+    private String applyUserId;
+
+    /** 处理该订单的用户id */
+    private String dealUserId;
+
+    /** 设备问题描述 */
+    @NotNull(message = "问题描述不能为空",groups = {SubmitRepairOrder.class})
+    private String description;
+
+    /** 状态代码 0待维修，1维修中，2已维修，3无法维修*/
+    private Integer statusCode;
+
+    /** 预计维修时间 */
+    @NotNull(message = "预计时间不能为空",groups = {SubmitRepairOrder.class})
+    private Date expectedTime;
+
+    /** 订单创建时间 */
+    private Date createTime;
+
+    /** 订单更新时间 */
+    private Date updateTime;
+
+}
